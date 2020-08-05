5071378df37998386a922712671e0e42d13eff7c
Marvis Leung <msl@genisys.com.au>
2020-08-06 02:33:14 +1000

    ClearDB Host configuration

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => $url,
             // 'host' => env('DB_HOST', '127.0.0.1'),
             // 'port' => env('DB_PORT', '3306'),
             // 'database' => env('DB_DATABASE', 'forge'),
