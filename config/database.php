5111268f3235daa904d38e32f26055b48208100e
Marvis Leung <msl@genisys.com.au>
2020-08-06 02:32:25 +1000

    Database configuration ClearDB

--- config/database.php
+++ config/database.php
@@ -61,6 +61,7 @@ return [
             'database'  => $database,
             'username'  => $username,
             'password'  => $password,
+            'port' => env('DB_PORT', '3306'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
